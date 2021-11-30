82fd6dc15d9e7602491ab6f8fa9511604ea5d7fb
Jakob Krebs <jakob@krbs.me>
2021-11-30 10:48:50 +0100

    remove debug prints

--- helper.cc
+++ helper.cc
@@ -7,10 +7,6 @@
 extern off_t bcm2835_peripherals_base;
 
 uint32_t bcm2835_peri_read(volatile uint32_t *paddr) {
-
-  volatile l4_uint32_t *offset = paddr - bcm2835_peripherals_base;
-  printf("reading from paddr 0x%4X, offset 0x%4X\n", paddr, offset);
-  fflush(NULL);
   return spi->read<uint32_t>((l4_uint64_t)paddr - 0x200000);
 }
 
@@ -21,9 +17,6 @@ uint32_t bcm2835_peri_read(volatile uint32_t *paddr) {
  * The MB can be explicit, or one of the barrier read/write calls.
  */
 uint32_t bcm2835_peri_read_nb(volatile uint32_t *paddr) {
-  volatile l4_uint32_t *offset = paddr - bcm2835_peripherals_base;
-  printf("reading from paddr 0x%4X, offset 0x%4X\n", paddr, offset);
-  fflush(NULL);
   return spi->read<uint32_t>((l4_uint64_t)paddr - 0x200000);
 }
 
@@ -31,16 +24,10 @@ uint32_t bcm2835_peri_read_nb(volatile uint32_t *paddr) {
  */
 
 void bcm2835_peri_write(volatile uint32_t *paddr, uint32_t value) {
-  volatile l4_uint32_t *offset = paddr - bcm2835_peripherals_base;
-  printf("writing 0x%4X to paddr 0x%4X, offset 0x%4X\n", value, paddr, offset);
-  fflush(NULL);
   spi->write((l4_uint64_t)paddr - 0x200000, value);
 }
 
 /* write to peripheral without the write barrier */
 void bcm2835_peri_write_nb(volatile uint32_t *paddr, uint32_t value) {
-  volatile l4_uint32_t *offset = paddr - bcm2835_peripherals_base;
-  printf("writing 0x%4X to paddr 0x%4X, offset 0x%4X\n", value, paddr, offset);
-  fflush(NULL);
   spi->write((l4_uint64_t)paddr - 0x200000, value);
 }
